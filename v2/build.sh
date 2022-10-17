73b3fa8c0ec83ce69322662e58f9848ef8a5f487
Helen Cristina Ferreira <helencristina@google.com>
2022-10-17 21:03:09 +0000

    Add tests build step. Remove params for RC, Beam 2.42.0 is released

--- v2/build.sh
+++ v2/build.sh
@@ -4,7 +4,4 @@ mvn clean package -Dimage=${TARGET_GCR_IMAGE}      \
 	-Dbase-container-image.version=${BASE_CONTAINER_IMAGE_VERSION} \
 	-Dapp-root=${APP_ROOT}        \
 	-Dcommand-spec=${COMMAND_SPEC}   \
-	-am -pl ${TEMPLATE_MODULE} -DskipTests=true \
-	-Pvalidaterelease \
-	-Dbeam.version=2.42.0 \
-	-Dbeam.release.repo=https://repository.apache.org/content/repositories/orgapachebeam-1286
+	-am -pl ${TEMPLATE_MODULE} -DskipTests=true
