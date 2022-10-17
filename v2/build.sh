4305dbc50e8cd4ec0ebf64a0ec655920d613e632
Helen Cristina Ferreira <helencristina@google.com>
2022-10-17 21:31:18 +0000

    Add missing build step. Remove broken unit tests

--- v2/build.sh
+++ v2/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+mvn clean install -f ../it/pom.xml
+
 mvn clean package -Dimage=${TARGET_GCR_IMAGE}      \
 	-Dbase-container-image=${BASE_CONTAINER_IMAGE}  \
 	-Dbase-container-image.version=${BASE_CONTAINER_IMAGE_VERSION} \
